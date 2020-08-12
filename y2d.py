2e7f51dc4ae24dd7864b1c6185990158028ee8d7
meomkarchavan <chavanomkar245@gmail.com>
2020-08-12 18:15:36 +0530

    comments added

--- y2d.py
+++ y2d.py
@@ -1,32 +1,31 @@
-# pip install pytube3
-#This will install the pytube3 which will be required.
 from helper import humanbytes,fileExists,urlChecker,pathExistsElseCreate,connect
 from pytube import YouTube
-import time
 import os
 
 # Asking for all the video links
 def main():
+    # check is user is conncted to internet
     if not connect():
         print('No Internet Connection!\nExiting....')
-        exit()
+        exit() #exit is not
     else:
-        
         while True:
+            # check is user want to download multiple files
             multiple=input('\nDo you want to dowanlod multiple videos?(y/n): ').lower()
             if(multiple==('y')):
                 while True:
+                    # get txt file from user
                     via_txt=input('\nDo you want to send links via a txt file?(y/n)').lower()
                     if(via_txt=='y'):
                         while True:
                             path=input('\nEnter the path of txt file with video links on each line: ')
                             if(fileExists):
-                                links=[]
-                                links=open(path, "r").read().splitlines()
-                                break
+                                # urlChecker checks if the url is valid youtube link
+                                links=[url for url in open('Example_links.txt', "r").read().splitlines() if urlChecker(url)]                                break
                             else:
                                 print('File Not Found!, Please Check the file path')
                     elif(via_txt=='n'):
+                        # get one by one link from user
                         while True:
                             try:
                                 n = int(input("Enter the number of youtube videos to download:   "))
@@ -45,6 +44,7 @@ def main():
                     else:
                         print('Invalid Input!')
             elif(multiple=='n'):
+                # get single link from user
                 while True:
                     print("\nEnter the link of Video:")
                     url=input()
@@ -63,20 +63,22 @@ def main():
                 break
             else:print('Invalid Path!')
 
-        #Showing all details for videos and downloading them one by one
         for i in range(0,len(links)):
             link = links[i]
             print("\nGetting Details of Video")
+            # get video object
             yt = YouTube(link)
             os.system('cls')
             print("\nDetails for Video","\n")
             print("Title:   ",yt.title)
             print("Length of video:  ",yt.length,"seconds")
+            # get progressive streams i.e audio+video
             stream_ = yt.streams.filter(progressive=True)
             stream=str(stream_)
             stream = stream[1:]
             stream = stream[:-1]
             streamlist = stream.split(", ")
+            # display available options
             print("\nAll available options for downloads:\n")
             for i in range(0,len(streamlist)):
                 st = streamlist[i].split(" ")
@@ -86,6 +88,5 @@ def main():
             print("\nDownloading...")
             ys.download(SAVE_PATH)
             print("\nDownload completed!!")
-
 if __name__ == '__main__':
     main()
